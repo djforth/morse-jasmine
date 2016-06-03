00c64c31bbfef8deb9bcbef0b9a9668719e13068
Adrian Stainforth <adrian@morsedigital.com>
2016-06-03 12:09:57 +0100

    fix entry

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports{
+module.exports = {
   check_calls: require("./check_calls")
   , check_multiple_calls: require("./check_multiple_calls")
   , create_elements: require("./create_elements")
-  , get_modules: require("./get_modules")
+  , get_modules: require("./get_module")
   , simulate_click: require("./simulate_click")
   , spy_manager: require("./spy_manager")
   , stub_chain_methods: require("./stub_chain_methods")
